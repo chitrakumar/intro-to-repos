ece1da7569525bd1cfe746bab851169c47a2d8a4
chitrakumar <chitrakumar>
2022-02-01 17:50:03 +0000

    repo demo
    This is a commit to demo the repos functionality!

--- temperatures.py
+++ temperatures.py
@@ -6,7 +6,7 @@
 
 # COMMAND ----------
 
-from my_lib.my_funcs import *
+#from my_lib.my_funcs import *
 
 # COMMAND ----------
 
@@ -52,3 +52,8 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is a new markdown comment!
